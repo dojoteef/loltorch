612121d01cc8afa947e8f2fd298cb365ce76e5e3
Nader Akoury <dojoteef@gmail.com>
2016-07-15 10:06:52 -0700

    Multiple improvements for fetching match data

--- getmatches.lua
+++ getmatches.lua
@@ -1,8 +1,10 @@
 local cjson = require('cjson')
+local dataset = require('dataset')
 local file = require('pl.file')
 local lol = require('lol')
 local path = require('pl.path')
 local stringx = require('pl.stringx')
+local tds = require('tds')
 local torch = require('torch')
 
 local cmd = torch.CmdLine()
@@ -10,9 +12,11 @@ cmd:text()
 cmd:text('Get matches from the League of Legends API')
 cmd:text()
 cmd:text('Options')
+cmd:option('-cutoff','6.11','the lowest version to pull matches for')
 cmd:option('-region','na','name of the region to get matches from')
 cmd:option('-outdir','matches','the directory to store the matches in')
 cmd:option('-tier','challenger','what tier matches to get, i.e. challenger, master, etc')
+cmd:option('-verbose',false,'whether to have verbose output')
 cmd:text()
 
 -- parse input params
@@ -32,54 +36,67 @@ local api = lol.api('.keys/devel', opt.region, '.cache', {
 local league = lol.league(api)
 local match = lol.match(api)
 local matchlist = lol.matchlist(api)
+local cutoff = dataset.parseVersion(opt.cutoff)
 
-local ignoreList
 local ignoreFile = path.join(opt.outdir, opt.region, '.ignorelist')
-if path.exists(ignoreFile) then
-    local data = file.read(ignoreFile)
-    local ok,list = pcall(function() return cjson.decode(data) end)
-    if ok then
-        ignoreList = list
-    end
-end
-ignoreList = ignoreList or {}
+local ignoreList = path.exists(ignoreFile) and torch.load(ignoreFile) or tds.Hash()
 
-local function onMatchResponse(res, code, matchId)
+local function onMatchResponse(res, code, matchData)
     -- Status 200 means success and code being nil means we retrieved the data from the cache
     if code ~= nil and code ~= 200 then
         if code == 404 then
             -- while this isn't very efficient if there are a large number of missing matches that doesn't
             -- seem to be the case in practice so opting for a quick solution
-            ignoreList[tostring(matchId)] = true
-            file.write(ignoreFile, cjson.encode(ignoreList))
+            ignoreList[tostring(matchData.id)] = true
+            torch.save(ignoreFile, ignoreList)
         end
 
-        print('Received error ('..code..') trying to retreive match: '..stringx.shorten(cjson.encode(res), 100))
+        if opt.verbose then
+            print('Received error ('..code..') trying to retreive match: '..stringx.shorten(cjson.encode(res), 100))
+        end
         return
     end
 
-    if res.matchId and res.region then
-        file.write(path.join(opt.outdir, res.region, res.matchId..'.json'), cjson.encode(res))
+    if not (res.matchId and res.region and res.matchVersion) then
+        return
+    end
+
+    local version = dataset.parseVersion(res.matchVersion)
+    local matchfile = path.join(opt.outdir, res.region, res.matchId..'.json')
+    if not version or dataset.compareVersion(version, cutoff) then
+        matchData.cutoff = true
+    else
+        file.write(matchfile, cjson.encode(res))
     end
 end
 
-local function onMatchResponseDecorator(matchId)
+local function onMatchResponseDecorator(matchData)
     return function (res, code)
-        onMatchResponse(res, code, matchId)
+        onMatchResponse(res, code, matchData)
     end
 end
 
 local function onMatchlistResponse(res, code)
     -- Status 200 means success and code being nil means we retrieved the data from the cache
     if code ~= nil and code ~= 200 then
-        print('Received error ('..code..') trying to retreive matchlist: '..stringx.shorten(cjson.encode(res), 100))
+        if opt.verbose then
+            print('Received error ('..code..') trying to retreive matchlist: '..stringx.shorten(cjson.encode(res), 100))
+        end
         return
     end
 
+    -- Matches are received in descending chronological order (newest matches
+    -- first), so keep getting matches until you run into a match with a
+    -- version that is too old at which point move on to the next summoner.
     if res.matches then
         for _, entry in pairs(res.matches) do
+            local matchData = {id=entry.matchId}
             if not ignoreList[tostring(entry.matchId)] then
-                match:getById(entry.matchId, {callback=onMatchResponseDecorator(entry.matchId)})
+                match:getById(entry.matchId, {callback=onMatchResponseDecorator(matchData)})
+            end
+
+            if matchData.cutoff then
+                break
             end
         end
     end
@@ -88,7 +105,9 @@ end
 local function onLeagueResponse(res, code)
     -- Status 200 means success and code being nil means we retrieved the data from the cache
     if code ~= nil and code ~= 200 then
-        print('Received error ('..code..') trying to retreive league: '..stringx.shorten(cjson.encode(res), 100))
+        if opt.verbose then
+            print('Received error ('..code..') trying to retreive league: '..stringx.shorten(cjson.encode(res), 100))
+        end
         return
     end
 
